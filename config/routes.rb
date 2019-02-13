e59a827143b8e30eeba0d84604d2bf741fc6271f
anhek <shynn@icloud.com>
2019-02-13 20:53:44 +0100

    update: routes and apikey names

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show, :edit]
   resources :events, only: [:index, :new, :show, :create, :destroy] do
-    resources :participations, only: [:new, :index, :create, :destroy]
+    resources :participations, only: [:new, :index, :show, :create, :destroy]
   end
-  resources :charges
 end
